f00d08a4c237ca4d1dab6dcbf79102d718eb41c9
Sydney Kuhn <90277234+sydneykuhn@users.noreply.github.com>
2021-11-08 12:22:26 -0500

    #1

---
+++ code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Created by: Sydney Kuhn
+# Created on: Nov 2020
+# This program is the "Breakout" game on the PyBadge
+
+import ugame
+import stage
+
+
+def main():
+    # this function is the main game game_scene
+
+    print("\n\n\n") # 3 blank lines
+    print("Hello, World!")
+
+    # repeat forever, game loop
+    while True:
+        pass # just a placeholder for now
+
+if __name__ == "__main__":
+    game_scene()
